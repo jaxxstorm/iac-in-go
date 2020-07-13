cd73010bbd0b9a1a2048330060ebaca907144479
Lee Briggs <lee@leebriggs.co.uk>
2020-07-13 10:20:46 -0700

    add vpc

---
+++ vpc/go.mod
@@ -0,0 +1,8 @@
+module vpc.go
+
+go 1.14
+
+require (
+	github.com/jaxxstorm/pulumi-aws-vpc/go v0.0.0-20200712170006-0d1a95681dd5
+	github.com/pulumi/pulumi/sdk/v2 v2.6.1
+)
---
+++ vpc/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	vpc "github.com/jaxxstorm/pulumi-aws-vpc/go"
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+
+		awsVpc, err := vpc.NewVpc(ctx, "lbriggs", vpc.Args{
+			BaseCidr: "172.1.0.0/16",
+			Description: "lbriggs-vpc",
+			ZoneName: "aws.lbrlabs.com",
+			AvailabilityZoneNames: pulumi.StringArray{
+				pulumi.String("us-west-2a"), // this is dev, so restrict to one AZ for cost effectiveness
+			},
+			BaseTags: pulumi.StringMap{
+				"Owner": pulumi.String("lbriggs"),
+			},
+			Endpoints: vpc.Endpoints{
+				S3: true,
+				DynamoDB: true,
+			},
+		})
+
+		if err != nil {
+			return err
+		}
+
+		ctx.Export("id", awsVpc.ID)
+		ctx.Export("arn", awsVpc.Arn)
+		return nil
+	})
+}

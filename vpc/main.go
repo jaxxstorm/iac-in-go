2605cbddcd51b9b65d55884e66dafdec73f7becf
Lee Briggs <lee@leebriggs.co.uk>
2020-07-13 12:51:14 -0700

    use more AZs

--- vpc/main.go
+++ vpc/main.go
@@ -9,17 +9,19 @@ func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 
 		awsVpc, err := vpc.NewVpc(ctx, "lbriggs", vpc.Args{
-			BaseCidr: "172.1.0.0/16",
+			BaseCidr:    "172.1.0.0/16",
 			Description: "lbriggs-vpc",
-			ZoneName: "aws.lbrlabs.com",
+			ZoneName:    "aws.lbrlabs.com",
 			AvailabilityZoneNames: pulumi.StringArray{
-				pulumi.String("us-west-2a"), // this is dev, so restrict to one AZ for cost effectiveness
+				pulumi.String("us-west-2a"),
+				pulumi.String("us-west-2b"),
+				pulumi.String("us-west-2c"),
 			},
 			BaseTags: pulumi.StringMap{
 				"Owner": pulumi.String("lbriggs"),
 			},
 			Endpoints: vpc.Endpoints{
-				S3: true,
+				S3:       true,
 				DynamoDB: true,
 			},
 		})
